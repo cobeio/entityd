9830d020893166d5f82d36ac9ec7e87a9f2d2ede
Matt Porter <mporter@abilisoft.com>
2014-11-21 10:43:17 +0000

    Add state to endpointme

--- entityd/endpointme.py
+++ entityd/endpointme.py
@@ -20,8 +20,38 @@ class EndpointEntity:
     """Plugin to generate endpoint MEs."""
 
     def __init__(self):
-        self.endpoint_uuid = None
+        self.known_uuids = {}
         self.session = None
+
+    @staticmethod
+    @entityd.pm.hookimpl
+    def entityd_configure(config):
+        """Register the Process Monitored Entity."""
+        config.addentity('Endpoint', 'entityd.endpointme.EndpointEntity')
+
+    @entityd.pm.hookimpl
+    def entityd_sessionstart(self, session):
+        """Load up all the known endpoint UUIDs."""
+        self.session = session
+        loaded_values = \
+            self.session.pluginmanager.hooks.entityd_kvstore_getmany(
+                key_begins_with='entityd.endpointme:'
+            )
+        if loaded_values:
+            self.known_uuids = loaded_values
+        else:
+            self.known_uuids = {}
+
+    @entityd.pm.hookimpl
+    def entityd_sessionfinish(self):
+        """Store out all our known endpoint UUIDs."""
+        self.session.pluginmanager.hooks.entityd_kvstore_deletemany(
+            key_begins_with='entityd.endpointme:'
+        )
+
+        self.session.pluginmanager.hooks.entityd_kvstore_addmany(
+            values=self.known_uuids
+        )
 
     @entityd.pm.hookimpl
     def entityd_find_entity(self, name, attrs):
@@ -32,18 +62,35 @@ class EndpointEntity:
             return self.endpoints()
 
     @staticmethod
-    def get_uuid():
-        """Get a uuid for endpoint."""
+    def _cache_key(fd):
+        """Get a standard cache key for a process entity."""
+        return 'entityd.endpointme:{}'.format(fd)
 
-        value = uuid.uuid4().hex
-        return value
+    def get_uuid(self, fd):
+        """Get a uuid for this process if one exists, else generate one.
+
+        :param pid: Process ID
+        :param start_time: Start time of the process
+        """
+        key = self._cache_key(fd)
+        if self.known_uuids and key in self.known_uuids:
+            return self.known_uuids[key]
+        else:
+            value = uuid.uuid4().hex
+            self.known_uuids[key] = value
+            return value
 
     def endpoints(self):
-        """Generator of all endpoints."""
+        """Generator of all endpoints.
+
+        Yields all connections of active processes.
+        """
         processes, = self.session.pluginmanager.hooks.entityd_find_entity(
             name='Process', attrs=None)
 
         for process in processes:
+            if 'delete' in process and process['delete']:
+                continue
             yield from self.endpoints_for_process(process)
 
     def endpoints_for_process(self, proc):
@@ -56,7 +103,7 @@ class EndpointEntity:
         for conn in endpoints:
             yield {
                 'type': 'Endpoint',
-                'uuid': self.get_uuid(),
+                'uuid': self.get_uuid(conn.fd),
                 'attrs': {
                     'local_addr': conn.laddr,
                     'remote_addr': conn.raddr

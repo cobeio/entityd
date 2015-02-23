bf0f6a4e409f470d9610f5284918042c97fd1fb1
Ben Secretan <bsecretan@abilisoft.com>
2015-02-23 10:23:54 +0000

    Add declerative entity to builtin plugins list.

--- entityd/__main__.py
+++ entityd/__main__.py
@@ -18,7 +18,7 @@ import entityd.version
 #: These plugins are always loaded first and in order.
 BUILTIN_PLUGIN_NAMES = ['entityd.' + n for n in
                         ['core', 'mesend', 'kvstore', 'hostme', 'processme',
-                         'endpointme']]
+                         'endpointme', 'declentity']]
 
 
 log = logging.getLogger('bootstrap')

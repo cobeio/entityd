7ca8ca3595d9819a8830ec7b28b73df294b3e8dc
Matt Porter <mporter@abilisoft.com>
2015-03-06 15:14:00 +0000

    Tidy up some straggling deletes

--- tests/test_processme.py
+++ tests/test_processme.py
@@ -43,8 +43,6 @@ def test_find_entity(procent, session, kvstore):  # pylint: disable=unused-argum
     count = 0
     for entity in entities:
         assert entity.metype == 'Process'
-        if entity.deleted:
-            continue
         assert entity.attrs.get('starttime').value
         count += 1
     assert count
@@ -98,18 +96,6 @@ def test_find_entity_with_binary(procent, session, kvstore):  # pylint: disable=
     proc = next(entities)
     assert proc.metype == 'Process'
     assert proc.attrs.get('binary').value == 'py.test'
-
-
-def test_deleted_filtered(procent, session, kvstore, monkeypatch):  # pylint: disable=unused-argument
-    procent.entityd_sessionstart(session)
-    update = entityd.entityupdate.EntityUpdate('Process', ueid=b'fedcba')
-    update.attrs.set('binary', 'mybinary')
-    deleted = entityd.entityupdate.EntityUpdate('Process', ueid=b'abcdef')
-    deleted.delete()
-    gen = (update for update in [update, deleted])
-    monkeypatch.setattr(procent, 'processes', pytest.Mock(return_value=gen))
-    assert list(procent.entityd_find_entity(
-        'Process', {'binary': 'mybinary'})) == [update]
 
 
 def test_get_ueid_new(kvstore, session, procent):  # pylint: disable=unused-argument
@@ -227,8 +213,6 @@ def test_processes_deleted(procent, proctable, monkeypatch, session, kvstore):  
     # Extract the ME for the py.test process
     gen = procent.processes()
     for me in gen:
-        if me.deleted:
-            continue
         if me.attrs.get('pid').value == os.getppid():
             pprocme = me
 

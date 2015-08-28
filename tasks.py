126df44f0a16fe69a6ddf83575d3c1113124e151
Ben Secretan <bsecretan@abilisoft.com>
2015-08-28 09:04:12 +0100

    Add invoke task to generate certificates for ZMQ authentication.

--- tasks.py
+++ tasks.py
@@ -5,9 +5,13 @@ no issues at all when submitting a pullrequest.
 
 """
 
+import os
+import pathlib
 import xml.etree.ElementTree as etree
 
+import act
 import invoke
+import zmq.auth
 
 
 @invoke.task
@@ -45,3 +49,35 @@ def check():
     pytest()
     if error:
         raise error
+
+
+@invoke.task(help={'dirpath': 'The base path to install keys under. Keys will'
+                              ' be installed at `<dirpath>/entityd/keys` with'
+                              ' any necessary directories being created. If'
+                              ' not provided the default is '
+                              '`<env>/etc/entityd/keys`.',
+                   'force': 'Force overwriting of existing keys.',
+                   'dry-run': 'Do a dry-run without making any keys.'})
+def certificates(dirpath=None, force=False, dry_run=False):
+    """Create certificates for ZMQ authentication."""
+    dirpath = os.path.expanduser(dirpath) if dirpath else act.fsloc.sysconfdir
+    dirpath = pathlib.Path(dirpath).absolute().joinpath('entityd', 'keys')
+
+    for parent in [dirpath] + list(dirpath.parents):
+        if parent.exists() and not parent.is_dir():
+            print('Abort. Path exists and is not a directory: '
+                  '{}'.format(parent))
+            return
+    if not dirpath.exists():
+        print('Directory does not exist, creating: {}'.format(dirpath))
+        if not dry_run:
+            dirpath.mkdir(parents=True)
+    for keyname in ['entityd.key', 'entityd.key_secret']:
+        if dirpath.joinpath(keyname).exists() and not force:
+            print('Abort. Key exists: {}'.format(dirpath.joinpath(keyname)))
+            print('Use --force to overwrite any existing keys.')
+            return
+    if not dry_run:
+        modeld_public, _ = zmq.auth.create_certificates(str(dirpath),
+                                                        'entityd')
+    print('Created entityd.key and entityd.key_secret in {}'.format(dirpath))

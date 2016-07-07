422139177f9aa614173b381087ba8f9dd7060ae7
Floris Bruynooghe <flub@devork.be>
2016-07-07 11:36:43 +0100

    Remove conda recipes
    They now below in our pkgs repo instead.

--- conda/config-recipe/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dest=$PREFIX/share/recipes/entityd-config/
-mkdir -p $dest
-
-for path in conda/config/* ; do
-    cp -f "$path" "$dest/$(basename $path)"
-done
--- conda/config/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-edd template conda/config/entityd.supervisord.conf > conda/config/entityd.conf
-config-files build \
-    conda/config/entityd.conf etc/supervisord.conf.d/entityd.conf

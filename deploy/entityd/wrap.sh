66a569feb7ae4a87a51c4b1106d1164c6a61f93b
Steve Clark <steve@cobe.io>
2017-07-24 14:32:05 +0100

    Fixing path in wrap.sh

--- deploy/entityd/wrap.sh
+++ deploy/entityd/wrap.sh
@@ -5,4 +5,4 @@ set -e
 mkdir -p /usr/etc/entityd/keys
 cp /opt/entityd/keys/entityd.key-secret /usr/etc/entityd/keys/entityd.key_secret
 cp /opt/entityd/keys/modeld.key /usr/etc/entityd/keys/modeld.key
-exec /usr/local/bin/entityd "$@"
+exec /venvs/entityd/bin/entityd "$@"

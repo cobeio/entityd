f7102e604ca5e99d5aadf925635eec5018ee02a5
Oliver Ainsworth <oainsworth@abilisoft.com>
2017-08-10 13:37:39 +0100

    Ping beacon whilst agent is running

--- deploy/entityd/wrap.sh
+++ deploy/entityd/wrap.sh
@@ -5,4 +5,25 @@ set -e
 mkdir -p /opt/entityd/etc/entityd/keys
 cp /opt/entityd/keys/entityd.key-secret /opt/entityd/etc/entityd/keys/entityd.key_secret
 cp /opt/entityd/keys/modeld.key /opt/entityd/etc/entityd/keys/modeld.key
-exec /opt/entityd/bin/entityd "$@"
+
+do_curl() {
+    curl -sS --output /dev/null "$@" || :
+}
+
+BEACON_SCOPE="$COBE_TOPOLOGY_INSTANCE"
+if [ -z "$BEACON_SCOPE" ]; then
+    BEACON_SCOPE=$(uuid -v1)
+fi
+BEACON_ID=$(uuid -v4)
+BEACON="A:$BEACON_SCOPE:$BEACON_ID"
+BEACON_URL="https://beacon.cobe.io/$BEACON"
+echo "BEACON: $BEACON_URL"
+entityd "$@" &
+AGENT_PID=$!
+trap "kill $AGENT_PID" INT TERM EXIT
+while [ -e "/proc/$AGENT_PID" ]; do
+    do_curl -X PUT "$BEACON_URL"
+    sleep 1s
+done
+do_curl -X DELETE "$BEACON_URL"
+
--- entityd.Dockerfile
+++ entityd.Dockerfile
@@ -25,6 +25,8 @@ WORKDIR /entityd
 RUN /opt/entityd/bin/pip3 install -e .
 
 ENV PATH $PATH:/opt/entityd/bin
+RUN apt-get install curl
+RUN apt-get install uuid
 COPY deploy/entityd/wrap.sh /usr/local/bin/wrap.sh
 ENTRYPOINT ["/usr/local/bin/wrap.sh"]
 CMD ["--help"]

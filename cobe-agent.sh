75d2b8edbcc24baaf759ea34652b99c63a1061aa
Matt Porter <mporter@abilisoft.com>
2017-09-11 14:59:00 +0100

    Remove this key copying now we have a parameter for keys

--- cobe-agent.sh
+++ cobe-agent.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-mkdir -p /opt/entityd/etc/entityd/keys
-cp /opt/entityd/keys/entityd.key-secret /opt/entityd/etc/entityd/keys/entityd.key_secret
-cp /opt/entityd/keys/modeld.key /opt/entityd/etc/entityd/keys/modeld.key
-
 do_curl() {
     curl -sS --output /dev/null "$@" || :
 }

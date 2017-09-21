bd8c036ef81780c761459c1729d26256116e9895
Steve Clark <steve@cobe.io>
2017-09-21 17:16:02 +0100

    Making a start with docker services

--- entityd/docker/swarm.py
+++ entityd/docker/swarm.py
@@ -15,7 +15,7 @@ log = logbook.Logger(__name__)
 
 class DockerSwarm:
     """An entity for the docker swarm."""
-    name = "Docker:Swarm"
+    name = 'Docker:Swarm'
 
     @entityd.pm.hookimpl
     def entityd_configure(self, config):
@@ -139,6 +139,77 @@ class DockerNode:
                 for node in client.nodes.list():
                     update = entityd.EntityUpdate(self.name)
                     update.label = node.attrs['Description']['Hostname']
+                    update.attrs.set('id', node.attrs['ID'],
+                                     traits={'entity:id'})
+
+                    update.attrs.set('node:id', node.attrs['ID'])
+                    update.attrs.set('node:role',
+                                     node.attrs['Spec']['Role'])
+                    update.attrs.set('node:availability',
+                                     node.attrs['Spec']['Availability'])
+                    update.attrs.set('node:labels',
+                                     node.attrs['Spec']['Labels'])
+                    update.attrs.set('node:state',
+                                     node.attrs['Status']['State'])
+                    update.attrs.set('node:address',
+                                     node.attrs['Status']['Addr'])
+                    update.attrs.set('node:version',
+                                     node.attrs['Version']['Index'])
+
+                    manager_attrs = node.attrs['ManagerStatus']
+                    update.attrs.set('node:manager:reachability',
+                                     manager_attrs['Reachability'])
+                    update.attrs.set('node:manager:leader',
+                                     manager_attrs['Leader'])
+                    update.attrs.set('node:manager:addr',
+                                     manager_attrs['Addr'])
+                    yield update
+            except APIError as error:
+                if error.status_code == 503:
+                    log.debug("Can't get node list on non manager nodes")
+                else:
+                    raise
+
+
+class DockerService:
+    """An entity for the docker node."""
+    name = "Docker:Service"
+
+    @entityd.pm.hookimpl
+    def entityd_configure(self, config):
+        """Register the Docker Node Entity."""
+        config.addentity(self.name, 'entityd.docker.swarm.DockerService')
+
+    @entityd.pm.hookimpl
+    def entityd_find_entity(self, name, attrs=None,
+                            include_ondemand=False):  # pylint: disable=unused-argument
+        """Find the docker node entities."""
+        if name == self.name:
+            if attrs is not None:
+                raise LookupError('Attribute based filtering not supported')
+            return self.generate_updates()
+
+    @classmethod
+    def get_ueid(cls, docker_node_id):
+        """Create a ueid for a docker node."""
+        entity = entityd.EntityUpdate(cls.name)
+        entity.attrs.set('id', docker_node_id, traits={'entity:id'})
+        return entity.ueid
+
+    def generate_updates(self):
+        """Generates the entity updates for the docker node."""
+        if not DockerClient.client_available():
+            return
+
+        client = DockerClient.get_client()
+        client_info = client.info()
+
+        if DockerSwarm.swarm_exists(client_info):
+            try:
+                for service in client.services.list():
+
+                    update = entityd.EntityUpdate(self.name)
+                    update.label = service.attrs['Description']['Hostname']
                     update.attrs.set('id', node.attrs['ID'],
                                      traits={'entity:id'})
 
